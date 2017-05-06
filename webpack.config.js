b652aec5832702a68f7b1dc0ad4255a134adeb76
Karen Ho <littlepinkrose12@gmail.com>
2017-05-06 13:15:25 -0700

    initial commit for client code

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var config = {
+  devtool: 'source-map',
+  entry: [
+    './src/app.js',
+    'webpack-hot-middleware/client'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      exclude: /node_modules/
+    }]
+  }
+};
+
+module.exports = config;
